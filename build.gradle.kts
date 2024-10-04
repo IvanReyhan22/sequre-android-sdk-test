1f49851cbceece6c7a54ed761366d8a64e5abb51
Ahmad Ivan Reyhan <ivanreyhan2002@gmail.com>
2024-10-04 09:34:06 +0700

    fix(gradle): using jdk 17 for env

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,17 @@
+buildscript{
+    dependencies{
+        classpath("com.android.tools.build:gradle:8.5.1")
+    }
+}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
+}
+
+task("clean"){
+    delete {
+        rootProject.buildDir
+    }
 }
